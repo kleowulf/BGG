80ff7cbb6f76bc66bdf16331b7e51a2e90da5d93
Kleowulf <claytond921@gmail.com>
2024-08-22 08:17:03 -0600

    troubleshooting api

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-fetch("https://boardgamegeek.com/xmlapi2/")
-.then(res => res.json()) // parse response as JSON
-.then(data => {
-  console.log(data)
+let baseUrl = "https://www.boardgamegeek.com/xmlapi/"
+let user = "Klewoulf"
+
+// fetch(`${baseUrl}/collection/${user}?own=1`)
+// .then(res => res.json()) // parse response as JSON
+// .then(data => {
+//   console.log(data)
+// })
+// .catch(err => {
+//     console.log(`error ${err}`)
+// });
+
+fetch(`${baseUrl}/collection/${user}?own=1`, {
+  mode:  'cors',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    key1: 'value1',
+    key2: 'value2'
+  })
 })
-.catch(err => {
-    console.log(`error ${err}`)
-});
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error(error));
